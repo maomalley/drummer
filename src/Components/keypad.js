8eaad34a8fa64b130e0af617d057d26820f677cd
meirianne <mauromall@yahoo.com>
2023-06-17 13:10:35 -0400

    css

--- src/Components/keypad.js
+++ src/Components/keypad.js
@@ -9,15 +9,15 @@ import React, {useState} from 'react';
  */
 function Keypad(props) {
     const buttons = [
-        ["heater1","https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"],
-        ["heater2","https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3"],
-        ["heater3","https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"],
-        ["heater4","https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3"],
-        ["clap","https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"],
-        ["openH","https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3"],
-        ["kickHat","https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"],
-        ["kick","https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"],
-        ["closedH","https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"]
+        ["heater1","https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3","q"],
+        ["heater2","https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3","w"],
+        ["heater3","https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3","e"],
+        ["heater4","https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3","a"],
+        ["clap","https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3","s"],
+        ["openH","https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3","d"],
+        ["kickHat","https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3","z"],
+        ["kick","https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3","x"],
+        ["closedH","https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3","c"]
         // "heater4","clap","openH","kickHat","kick","closedH"
     ];
     const [currAudio, setCurrAudio] = useState("");
@@ -128,12 +128,14 @@ function Keypad(props) {
     return (
         <div id="keypad">
             <div id="name">{currAudio}</div>
+            <div id="grid">
                 {buttons.map((button,index) =>
                     <button key={index} onKeyDown={(e) => handleKeyPress(e)} onClick={() => clickButton(button[0])}>
-                        {button[0]}
+                        {button[2]}
                         <audio src={button[1]} id={button[0]}></audio>
                     </button>
                 )}
+            </div>
         </div>
     );
 }
